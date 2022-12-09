f64bf71f64665e73a0472ebeebd07bc9713325b0
Eduardo Pacheco da Silva <domiedupac@gmail.com>
2022-12-09 15:15:26 -0300

    Commit Inicial

---
+++ avaltinnova/src/main/java/com/api/avaltinnova/AvaltinnovaApplication.java
@@ -0,0 +1,13 @@
+package com.api.avaltinnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaltinnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaltinnovaApplication.class, args);
+	}
+
+}
---
+++ avaltinnova/src/test/java/com/api/avaltinnova/AvaltinnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.avaltinnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaltinnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
